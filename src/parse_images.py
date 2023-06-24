9389b47e348edb3327b9c0cde4ff7ad54e0a7a46
SlickSpore <ettorecaccioli@gmail.com>
2023-06-24 12:19:27 +0200

    added image parser

---
+++ src/parse_images.py
@@ -0,0 +1,8 @@
+import os
+
+cwd = os.getcwd()
+os.chdir(f"{cwd}\\src\\assets\\ppm")
+
+for i in os.listdir():
+    os.system(f"python.exe {cwd}\\src\\ppm2ssg\\ppm2ssg_vga256.py {cwd}\\src\\assets\\ppm\\{i} {cwd}\\src\\assets\\ssg\\{i.split('.')[0]}.ssg")    
+    
